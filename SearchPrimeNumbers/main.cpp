33c2097cb683129656b091b5e9894e51deb91835
Oleg Umnov <oleg_umnov@mail.ru>
2022-11-16 19:09:05 +0300

    SearchOfPrimeNumbers v1.2 date 16.11.2022

--- SearchPrimeNumbers/main.cpp
+++ SearchPrimeNumbers/main.cpp
@@ -5,9 +5,33 @@
 
 #include "log_duration.h"
 
-//-----Количество простых чисел строго меньше n (n - натуральное число)
+//-----Поиск простых чисел строго меньше n (n - натуральное число)
 
 using namespace std;
+
+vector<int> Eratosfen(int n) {
+    LOG_DURATION("Eratosfen");
+
+    vector<int> lp(n);
+    for (int i = 0; i < n; ++i) {
+        lp[i] = 0;
+    }
+    vector<int> result(0);
+
+    for (int i = 2; i < n; ++i) {
+        if (lp[i] == 0) {
+            lp[i] = i;
+            result.push_back(i);
+        }
+
+        for(int p : result) {
+            if (p <= lp[i] && p*i <= n) {
+                lp[p*i] = p;
+            }
+        }
+    }
+    return result;
+}
 
 bool IsPrime1(int p) {
     int sp = sqrt(p);
@@ -132,43 +156,19 @@ void Experimental_4(int n) {
         }
     }
 
-    cout << endl;
-    //int* result[n];
-    int j = 0;
-    for (int i = 0; i < n; i += 1) {
-        if (*(pointer + i) != 0) {
-            //result[j] = (pointer + i);
-            cout << *(pointer + i) << " ";
-            ++j;
-        }
-    }
+//    cout << endl;
+//    //int* result[n];
+//    int j = 0;
+//    for (int i = 0; i < n; i += 1) {
+//        if (*(pointer + i) != 0) {
+//            //result[j] = (pointer + i);
+//            cout << *(pointer + i) << " ";
+//            ++j;
+//        }
+//    }
     cout << endl << "Количество найденных простых чисел = " << k << endl;
-    cout << endl << "Количество найденных простых чисел (повторно) = " << j << endl;
+    //cout << endl << "Количество найденных простых чисел (повторно) = " << j << endl;
     cout << "Calculate PN --OK--" << endl;
-}
-
-vector<int> Eratosfen(int n) {
-    LOG_DURATION("Eratosfen");
-
-    vector<int> lp(n);
-    for (int i = 0; i < n; ++i) {
-        lp[i] = 0;
-    }
-    vector<int> result(0);
-
-    for (int i = 2; i < n; ++i) {
-        if (lp[i] == 0) {
-            lp[i] = i;
-            result.push_back(i);
-        }
-
-        for(int p : result) {
-            if (p <= lp[i] && p*i <= n) {
-                lp[p*i] = p;
-            }
-        }
-    }
-    return result;
 }
 
 void OptimizedEratosfen(int n) {
@@ -196,22 +196,97 @@ void OptimizedEratosfen(int n) {
         }
     }
 
-    cout << endl;
+//    cout << endl;
+//    int j = 0;
+//    for (int i = 0; i < n/2; i += 1) {
+//        if (*(pointer + i) != 0) {
+//            cout << *(pointer + i) << " ";
+//            ++j;
+//        }
+//    }
+    cout << endl << "Количество найденных простых чисел = " << k << endl;
+    //cout << endl << "Количество найденных простых чисел (повторно) = " << j << endl;
+    cout << "Calculate PN --OK--" << endl;
+}
+
+vector<int> OptimizedEratosfen1(int n) {
+    LOG_DURATION("OptimizedEratosfen1");
+
+    vector<int> result(n/2 + 1);
+    result[0] = 2;
+    int k = 1;
+
+    //for (int i = 3; i < n; i += 2) { lp[i] = 0; }
+
+    for (int i = 3; i < n; i += 2) {
+        result[(int)i/2] = i;
+        ++k;
+    }
+
+    int maxFactor = (int)sqrt(n);
+
+    for (int i = 3; i <= maxFactor; i += 2) {
+        if (result[(int)i/2] != 0) {
+            for (int ii = i*i; ii < n; ii += 2*i) {
+                if (result[(int)ii/2] != 0) {
+                    result[(int)ii/2] = 0;
+                    --k;
+                }
+            }
+        }
+    }
+
+    vector<int> res(k);
+
+    int size = result.size();
     int j = 0;
-    for (int i = 0; i < n/2; i += 1) {
-        if (*(pointer + i) != 0) {
-            cout << *(pointer + i) << " ";
+    for (int i = 0; i < size; i += 1) {
+        if (result[i] != 0) {
+            res[j] = result[i];
             ++j;
         }
     }
-    cout << endl << "Количество найденных простых чисел = " << k << endl;
-    cout << endl << "Количество найденных простых чисел (повторно) = " << j << endl;
-    cout << "Calculate PN --OK--" << endl;
+    return res;
+}
+
+void OptimizedEratosfen2(int n) {
+    LOG_DURATION("OptimizedEratosfen2");
+
+    static bool result[500'000'000];
+
+    int k = n/2;
+    for (int i = 1; i < n/2 + 1; ++i) {
+        result[i] = true;
+    }
+
+    int maxFactor = (int)sqrt(n);
+
+    for (int i = 3; i <= maxFactor; i += 2) {
+        if (result[i/2]) {
+            for (int ii = i*i; ii < n; ii += 2*i) {
+                if (result[ii/2]) {
+                    result[ii/2] = false;
+                    --k;
+                }
+            }
+        }
+    }
+
+//    int j = 1;
+//    for (int i = 1; i < n/2; i += 1) {
+//        if (result[i]) {
+//            ++j;
+//        }
+//    }
+
+    cout << "Количество найденных простых чисел = " << k << endl;
+    //cout << "Количество найденных простых чисел (повторно) = " << j << endl;
+    cout << endl << "Calculate PN --OK--" << endl << endl;
 }
 
 void Print(const vector<int>& prime_numbers) {
     cout << "Количество найденных простых чисел = " << prime_numbers.size() << endl;
-    for (int i = 0; i < prime_numbers.size(); ++i) { cout << prime_numbers[i] << " "; }
+    //for (int i = 0; i < prime_numbers.size(); ++i) { cout << prime_numbers[i] << " "; }
     cout << endl << "Calculate PN --OK--" << endl << endl;
 }
 
@@ -220,14 +295,16 @@ int main() {
     cin >> n;
     cout << endl << "Поиск простых чисел от 2 до " << n << " --->" << endl;
 
-    Print(Eratosfen(n));
-    Print(Experimental_0(n));
-    Print(Experimental_1(n));
-    Print(Experimental_2(n));
-    Print(Experimental_3(n));
-    Experimental_4(n);
+    //Print(Eratosfen(n));
+//    Print(Experimental_0(n));
+//    Print(Experimental_1(n));
+//    Print(Experimental_2(n));
+//    Print(Experimental_3(n));
+//    Experimental_4(n);
 
     OptimizedEratosfen(n);
+    Print(OptimizedEratosfen1(n));
+    OptimizedEratosfen2(n);
 
     cout << endl;
 
